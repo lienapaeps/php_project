22ff9c3cf0aa86dfb25974b8c8b7de4cd056e4f5
lienapaeps <paepsliena@gmail.com>
2022-03-18 12:17:20 +0100

    feat: add basic register page

---
+++ register.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>IMD Showcase | Sign up</title>
+</head>
+
+<body class="container">
+
+    <div class="alert alert-danger">Here you can display errors</div>
+
+    <form action="" method="POST">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email adress</label>
+            <input type="email" name="email" id="email" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>
+        <div>
+            <button class="btn btn-primary" type="submit">Sign Up</button>
+        </div>
+    </form>
+
+    <div class="mt-3">
+        <a href="login.php">Already have a account? <span>Sign in</span></a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+</body>
+
+</html>

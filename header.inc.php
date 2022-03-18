0e475a362dbc4784c3128c1a2ecb46f6d9fbfd76
jeffasseur <r0846107@student.thomasmore.be>
2022-03-18 20:06:20 +0100

    feat: changed word 'logo' to Vibbar ++

--- header.inc.php
+++ header.inc.php
@@ -2,10 +2,10 @@
     <nav class="navbar navbar-expand-lg fixed-top bg-light">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon align-middle"><i class="fas fa-bars"></i></span>
+                <span class="navbar-toggler-icon"><i class="fas fa-bars align-middle"></i></span>
             </button>
 
-            <a class="navbar-brand" href="index.php">Vibbar ++</a>
+            <a class="navbar-brand" href="index.php"><strong>Vibbar ++</strong></a>
 
             <a class="offcanvas-title btn btn-primary" href="login.php">Sign in</a>
 
@@ -14,9 +14,14 @@
                     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">
                         <!-- <i class="fas fa-times"></i> -->
                     </button>
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Vibbar ++</h5>
                 </div>
                 <div class="offcanvas-body">
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-primary" type="submit"><i class="fas fa-search"></i></button>
+                    </form>
+
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -28,10 +33,6 @@
                             <a class="nav-link" href="#">Link</a>
                         </li>
                     </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-primary" type="submit"><i class="fas fa-search"></i></button>
-                    </form>
                 </div>
             </div>
         </div>

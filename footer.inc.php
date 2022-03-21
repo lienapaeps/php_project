aa969bf75879988c0b65a569c2cd971cc0753858
jeffasseur <r0846107@student.thomasmore.be>
2022-03-21 11:04:55 +0100

    fix: columns footer

--- footer.inc.php
+++ footer.inc.php
@@ -1,12 +1,12 @@
 <footer class="pt-4 bg-light mt-4">
     <div class="container pt-4">
     <div class="row py-4">
-        <div class="col-lg-3 mb-2">
+        <div class="col-lg-3 mb-4">
             <a href="index.php" class="d-inline-flex align-items-center mb-2 fs-3 text-decoration-none">
                 <strong>Vibbar ++</strong>
             </a>
             <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ad totam rem necessitatibus dignissimos quidem minus, obcaecati quia quibusdam laborum tempore odio.</small></p>
-            <ul class="nav">
+            <ul class="nav mb-2">
                 <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-facebook-f fa-lg"></i></a></li>
                 <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-instagram fa-lg"></i></a></li>
                 <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-twitter fa-lg"></i></a></li>
@@ -23,7 +23,7 @@
             </ul>
         </div>
 
-        <div class="col-6 col-lg-2 offset-lg-1 mb-2">
+        <div class="col-6 col-lg-2 mb-2">
             <h5>Developers</h5>
             <ul class="list-unstyled">
                 <li class="mb-2">

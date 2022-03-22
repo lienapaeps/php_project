32d1d5892e0c95b430efdf0ff3d971feee5be017
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-03-22 14:07:23 +0100

    style: migrate all css to style.css file

--- login.php
+++ login.php
@@ -29,55 +29,7 @@ if (!empty($_POST)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="css/style.css" rel="stylesheet">
-
-    <style>
-        /* X-Small devices (portrait phones, less than 576px)
-        No media query for `xs` since this is the default in Bootstrap */
-        body {
-            background-color: #eee;
-            margin-left: 2em;
-            margin-right: 2em;
-            margin-top: 4em;
-        }
-
-        .signin__form {
-            width: 100%;
-            background-color: #fff;
-            padding: 2em;
-            border-radius: 6px;
-        }
-
-        .form__title {
-            font-size: 1.5em;
-        }
-
-        .form__link {
-            text-align: center;
-        }
-
-        .form__link__password {
-            text-align: left;
-        }
-
-        /* Medium devices (tablets, 768px and up) */
-        @media only screen and (min-width: 768px) {
-            .signin__form {
-                width: 80%;
-                margin-left: auto;
-                margin-right: auto;
-            }
-        }
-
-        /* X-Large devices (large desktops, 1200px and up) */
-        @media only screen and (min-width: 1200px) {
-            .signin__form {
-                width: 65%;
-                margin-left: auto;
-                margin-right: auto;
-            }
-        }
-    </style>
+    <link href="css/style.css?<?php echo time() ?>" rel="stylesheet">
 
     <title>IMD Showcase | Log in</title>
 </head>

b22b2cc923cdb28d708fe6eda2de3ca262555f24
lienapaeps <paepsliena@gmail.com>
2022-03-22 14:28:57 +0100

    update: sql table users

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 18, 2022 at 04:56 PM
+-- Generation Time: Mar 22, 2022 at 01:27 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -31,16 +31,19 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL
+  `password` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `backup_mail` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profile_img` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'test', 'test@thomasmore.be', '$2y$13$GwONw5TALHvZIB1fOMP6EeZKj8rRLyUEvTtPC19kWjx9.AB2P0AaC'),
-(2, 'liena', 'liena@thomasmore.be', '$2y$13$MvcGQVyEVMAtd2yx0LRWteNuxn8m9jdmBZp48SXXQxuz/FmUQBB.K');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `backup_mail`, `profile_img`) VALUES
+(1, 'test', 'test@thomasmore.be', '$2y$13$GwONw5TALHvZIB1fOMP6EeZKj8rRLyUEvTtPC19kWjx9.AB2P0AaC', NULL, NULL),
+(2, 'liena', 'liena@thomasmore.be', '$2y$13$MvcGQVyEVMAtd2yx0LRWteNuxn8m9jdmBZp48SXXQxuz/FmUQBB.K', NULL, NULL),
+(3, 'p66liena', 'lienapaeps@thomasmore.be', '$2y$13$Vh0Ho.JENeUYGRr5fr.5reS5zAkUsblwJKdvgCKaSQs3q6wQ3TT2G', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -60,7 +63,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

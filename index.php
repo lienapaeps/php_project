836e480463743dc5901b8ba19b21842ba79849d2
lienapaeps <paepsliena@gmail.com>
2022-03-26 13:58:19 +0100

    feat: add empty state to index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -10,19 +11,19 @@
     <title>PHP Project</title>
 
     <!-- links to css and scripts -->
-        <!-- Bootstrap -->
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <!-- Fontawesome icons -->
+    <!-- Fontawesome icons -->
     <script src="https://kit.fontawesome.com/d5a678d06c.js" crossorigin="anonymous"></script>
-        <!-- Own CSS file -->
+    <!-- Own CSS file -->
     <link rel="stylesheet" href="css/style.css?<?php echo time() ?>">
 </head>
 
 <body>
 
-    <?php include_once("header.inc.php");?>
-    
+    <?php include_once("header.inc.php"); ?>
+
     <div class="container-fluid hero-empty-state py-4 border-bottom">
         <div class="hero-container text-center">
             <img class="w-50 mx-auto d-block" src="https://jeffasseur-visuals.be/wp-content/uploads/2022/03/20943391-scaled.jpg" alt="Hero image">
@@ -61,6 +62,12 @@
                 <a href="#" class="nav-link">Webdesign</a>
             </li>
         </ul>
+
+        <!-- empty state -->
+        <div>
+            <p>There is no content to show yet.</p>
+        </div>
+        <!-- end empty state -->
 
         <div class="d-flex flex-wrap justify-content-center gap-2 mb-4">
             <div href="#" class="card bg-light rounded-3" style="max-width: 24rem;">

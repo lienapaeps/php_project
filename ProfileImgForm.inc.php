e8030eeb282a138d190c7908b2fe859a3b729b92
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-04-04 17:14:13 +0200

    feat: add profile page html & css

--- ProfileImgForm.inc.php
+++ ProfileImgForm.inc.php
@@ -3,7 +3,7 @@
     if(isset($_POST["submitProfileImage"])){
         $profileImgName = $_FILES["profileImgUpload"]["name"];
         
-        $target = "images/" . $profileImgName;
+        $target = "images/" . $profileImgName; 
 
         move_uploaded_file($_FILES["profileImgUpload"]["tmp_name"], $target);
     } 
--- profile.php
+++ profile.php
@@ -10,6 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Profile Name</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
             <!-- Fontawesome icons -->
         <script src="https://kit.fontawesome.com/d5a678d06c.js" crossorigin="anonymous"></script>
@@ -50,26 +51,87 @@
     
         </section>
         <section class="profile">
-            <div class="profile__header row">
-                <div class="profile__imageBox col-sm-4 col-md-3 col-lg-3">
+            <div class="profile__header">
+                <div class="profile__imageBox ">
                     <img onclick="showForm" src="<?php echo $target; ?>" alt="profile image" class="profile__image">
                 </div>
-                <div class="profile__mainInfo col-sm-8 col-md-9 col-lg-9">
-                    <div class="profile__username"><h1>Username</h1></div>
+                <div class="profile__mainInfo ">
+                    <div class="profile__username"><h1>Josefien Jacobs</h1></div>
                     <div class="profile__course"><span>Interactive Multimedia Design</span></div>
-                    <div class="profile__description"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed id, molestiae voluptatibus est excepturi quod aliquam, ex quisquam neque ab illum atque iure ut maiores laboriosam sit nostrum odit laudantium.</p></div>
                 </div>
                 <div class="edit_button"></div>
             </div>
-            <div class="profile__nav row">
-                <a href="#" class="profile__link col-4 border "><span>Profile Info</span></a>
-                <a href="#" class="profile__link col-4 border "><span>Projects</span></a>
-                <a href="#" class="profile__link col-4 border "><span>Following</span></a>
+
+            <div class="profile__nav">
+                <div class="profile__navBox">
+                    <a href="#" class="profile__link" id="profilePersonalInfo"><span>Personal Info</span></a>
+                    <a href="#" class="profile__link" id="profileProjects"><span>Projects</span></a>
+                    <a href="#" class="profile__link" id="profileShowcase"><span>Showcase</span></a>
+                </div>
             </div>
-            <div class="profile__main border-rounded">
-                <div class="empty__state">
-                    <span>This user has no projects on display.</span>
-                    <img src="" alt="">
+            <div class="profile__main">
+                <div class="profile__showcase">
+                    <p class="nothing">No showcase available.</p>
+                </div>
+
+                <div class="profile__projects">
+                    <p class="nothing">No projects submitted.</p>
+                </div>
+                
+                <div class="profile__infos">
+                    <div class="profile__info description-area">
+                        <h2>Who am I?</h2>
+                        <p class="profile__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, est quae aliquam ipsa error recusandae, nobis dicta sed repellendus ea odit veniam quibusdam sit, doloremque deserunt delectus perferendis optio non!</p>
+                        <p class="profile__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, est quae aliquam ipsa error recusandae, nobis dicta sed repellendus ea odit veniam quibusdam sit, doloremque deserunt delectus perferendis optio non!</p>
+                    </div>
+                    <div class="profile__info extra-area">
+                        <h2>Information</h2>
+                        <div class="extra__box">
+                            <p class="extra__title">Projects uploaded</p>
+                            <p class="extra__number">6</p>
+                        </div>
+                        <div class="extra__box">
+                            <p class="extra__title">Following</p>
+                            <p class="extra__number">34</p>
+                        </div>
+                        <div class="extra__box">
+                            <p class="extra__title">Followers</p>
+                            <p class="extra__number">27</p>
+                        </div>
+                        <div class="extra__box">
+                            <p class="extra__title">Written comments</p>
+                            <p class="extra__number">5</p>
+                        </div>
+                        <div class="extra__box">
+                            <p class="extra__title">Total project likes</p>
+                            <p class="extra__number">214</p>
+                        </div>
+                        <div class="extra__box">
+                            <p class="extra__title">Most used tag</p>
+                            <p class="extra__number">#design</p>
+                        </div>
+                        
+                    </div>
+                    <div class="profile__info contact-area">
+                        <h2>Contact</h2>
+                        <a href="#" class="profile__infoLink">
+                            <i class="bi bi-envelope"></i>
+                            <p class="link__text">josjacobs@gmail.com</p>
+                        </a>
+                        <a href="#" class="profile__infoLink">
+                            <i class="bi bi-instagram"></i>
+                            <p class="link__text">Josefien Jacobs</p>
+                        </a>
+                        <a href="#" class="profile__infoLink">
+                            <i class="bi bi-facebook"></i>
+                            <p class="link__text">@JosJacobs</p>
+                        </a>
+                        <a href="#" class="profile__infoLink">
+                            <i class="bi bi-github"></i>
+                            <p class="link__text">Josefien_code</p>
+                        </a>
+                    </div>
+
                 </div>
             </div>
         </section>
@@ -79,7 +141,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script>
         let imageForm = document.querySelector(".image-upload__form");
-        
+
         document.querySelector(".profile__image").addEventListener("click", showForm);
         document.querySelector(".form-cancel").addEventListener("click", hideForm);
 
@@ -89,6 +151,39 @@
         function hideForm() {
             imageForm.style.display = "none";
         }
+
+        document.querySelector("#profilePersonalInfo").addEventListener("click", (navigate) => {
+            document.querySelector("#profilePersonalInfo").style.color = "var(--IMD_Blue)";
+            document.querySelector("#profileProjects").style.color = "var(--Black)";
+            document.querySelector("#profileShowcase").style.color = "var(--Black)";
+
+            document.querySelector(".profile__infos").style.display = "grid";
+            document.querySelector(".profile__projects").style.display = "none";
+            document.querySelector(".profile__showcase").style.display = "none";
+        });
+
+        document.querySelector("#profileProjects").addEventListener("click", (navigate) => {
+            document.querySelector("#profilePersonalInfo").style.color = "var(--Black)";
+            document.querySelector("#profileProjects").style.color = "var(--IMD_Blue)";
+            document.querySelector("#profileShowcase").style.color = "var(--Black)";
+
+            document.querySelector(".profile__infos").style.display = "none";
+            document.querySelector(".profile__projects").style.display = "block";
+            document.querySelector(".profile__showcase").style.display = "none";
+        });
+
+        document.querySelector("#profileShowcase").addEventListener("click", (navigate) => {
+            document.querySelector("#profilePersonalInfo").style.color = "var(--Black)";
+            document.querySelector("#profileProjects").style.color = "var(--Black)";
+            document.querySelector("#profileShowcase").style.color = "var(--IMD_Blue)";
+
+            document.querySelector(".profile__infos").style.display = "none";
+            document.querySelector(".profile__projects").style.display = "none";
+            document.querySelector(".profile__showcase").style.display = "block";
+        });
+
+
+
 
     </script>
 </body>

70f85769572768e27690f75c1e9570464a490331
lienapaeps <paepsliena@gmail.com>
2022-04-04 19:02:06 +0200

    update: sql file with demo project data

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 25, 2022 at 05:57 PM
+-- Generation Time: Apr 04, 2022 at 05:01 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -24,48 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `comment` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `time` datetime NOT NULL,
-  `warned` tinyint(1) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `project_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `following`
---
-
-CREATE TABLE `following` (
-  `id` int(11) NOT NULL,
-  `user_follower` int(11) NOT NULL,
-  `user_following` int(11) NOT NULL,
-  `time` datetime NOT NULL,
-  `active` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `likes`
---
-
-CREATE TABLE `likes` (
-  `id` int(11) NOT NULL,
-  `time` datetime NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `project_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `projects`
 --
 
@@ -82,99 +40,18 @@ CREATE TABLE `projects` (
   `project_content_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `project_content`
+-- Dumping data for table `projects`
 --
 
-CREATE TABLE `project_content` (
-  `id` int(11) NOT NULL,
-  `url` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alt` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content_type` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reported`
---
-
-CREATE TABLE `reported` (
-  `id` int(11) NOT NULL,
-  `time` datetime NOT NULL,
-  `message` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `social_links`
---
-
-CREATE TABLE `social_links` (
-  `id` int(11) NOT NULL,
-  `url` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `backup_email` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `profile_img` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `course` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `bio` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `muted` tinyint(1) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
-  `warned` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `backup_email`, `profile_img`, `course`, `bio`, `muted`, `admin`, `warned`) VALUES
-(1, 'p66liena', 'lienapaeps@thomasmore.be', '$2y$13$qxOtOj3qTUiV5Zf0IDJRluk8l32y32xs4DbaXZdBA31izJTfaVM4a', 'paepsliena@gmail.com', NULL, NULL, NULL, 0, 1, 0),
-(2, 'jefke', 'jeffasseur@thomasmore.be', '$2y$13$KcnN7rl.YQPeqdojESug9OxXwk3gcCZhy5rFLqo5lkB6ri.IiAt.i', NULL, NULL, NULL, NULL, 0, 1, 0),
-(3, 'rix', 'rickyheylen@thomasmore.be', '$2y$13$ygwBw3.gK7LjnDCrAfL3MuT1Ro5pDjZ6QXQq2.l9QPcn/liMBslUq', NULL, NULL, NULL, NULL, 0, 1, 0);
+INSERT INTO `projects` (`id`, `title`, `description`, `time`, `cover_img`, `warned`, `showcase`, `amount_views`, `user_id`, `project_content_id`) VALUES
+(1, 'Test', 'This is a test project', '2022-03-28 14:52:46', 'https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 0, 0, NULL, 1, 1),
+(2, 'Test2', 'This is another test', '2022-03-21 17:33:10', 'https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 0, 0, NULL, 2, 2),
+(3, 'Test3', 'This is another another test', '2022-03-14 14:30:14', 'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 0, 0, NULL, 3, 3);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `project_id` (`project_id`);
-
---
--- Indexes for table `following`
---
-ALTER TABLE `following`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `project_id` (`project_id`);
 
 --
 -- Indexes for table `projects`
@@ -185,81 +62,13 @@ ALTER TABLE `projects`
   ADD KEY `project_content_id` (`project_content_id`);
 
 --
--- Indexes for table `project_content`
---
-ALTER TABLE `project_content`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `reported`
---
-ALTER TABLE `reported`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `content_id` (`content_id`);
-
---
--- Indexes for table `social_links`
---
-ALTER TABLE `social_links`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `following`
---
-ALTER TABLE `following`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `project_content`
---
-ALTER TABLE `project_content`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `reported`
---
-ALTER TABLE `reported`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `social_links`
---
-ALTER TABLE `social_links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
@@ -267,39 +76,11 @@ ALTER TABLE `users`
 --
 
 --
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
-
---
--- Constraints for table `likes`
---
-ALTER TABLE `likes`
-  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
-
---
 -- Constraints for table `projects`
 --
 ALTER TABLE `projects`
   ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`project_content_id`) REFERENCES `project_content` (`id`);
-
---
--- Constraints for table `reported`
---
-ALTER TABLE `reported`
-  ADD CONSTRAINT `reported_ibfk_1` FOREIGN KEY (`content_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `reported_ibfk_2` FOREIGN KEY (`content_id`) REFERENCES `projects` (`id`),
-  ADD CONSTRAINT `reported_ibfk_3` FOREIGN KEY (`content_id`) REFERENCES `comments` (`id`);
-
---
--- Constraints for table `social_links`
---
-ALTER TABLE `social_links`
-  ADD CONSTRAINT `social_links_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

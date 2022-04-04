6b5d3cc2c9286454bf7f6352c5df77f82df359bb
lienapaeps <paepsliena@gmail.com>
2022-04-04 19:11:17 +0200

    feat: add scripts in head

--- login.php
+++ login.php
@@ -37,8 +37,17 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- links to css and scripts -->
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="css/style.css?<?php echo time() ?>" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- Fontawesome icons -->
+    <script src="https://kit.fontawesome.com/d5a678d06c.js" crossorigin="anonymous"></script>
+    <!-- Own CSS file -->
+    <link rel="stylesheet" href="css/style.css?<?php echo time() ?>">
+    <!-- Font: Museo Sans -->
+    <link rel="stylesheet" href="https://use.typekit.net/kkv2fee.css">
 
     <title>IMD Showcase | Log in</title>
 </head>

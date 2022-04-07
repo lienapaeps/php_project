b81164e27a2dacbbae0bfc76b51e2df95253a39f
jeffasseur <r0846107@student.thomasmore.be>
2022-04-07 15:57:48 +0200

    fix: change header style

--- header.inc.php
+++ header.inc.php
@@ -1,65 +1,60 @@
 <header>
-    <nav class="navbar navbar-expand-xl">
-        <div class="container-fluid px-4">
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon"><i class="fas fa-bars align-middle"></i></span>
-            </button>
+    <nav class="navbar navbar-expand-lg mb-3">
+        <div class="container-fluid">
+            <a href="index.php" class="navbar-brand logo">Vibar</a>
 
-            <a class="navbar-brand" href="index.php"><strong class="logo">Vibar</strong></a>
-
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">
-                        <!-- <i class="fas fa-times"></i> -->
-                    </button>
-                    <a class="offcanvas-title" id="offcanvasNavbarLabel"><strong class="logo">Vibar</strong></a>
-                </div>
-                <div class="offcanvas-body align-items-center">
-                    <form id="searchbar" class="d-flex searchbar mx-xl-4 mh-100" style="height: 40px">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-primary searchbar--btn" type="submit"><i class="fas fa-search"></i></button>
-                    </form>
-
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 my-4">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profile.php">Profile</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- offcanvas toggle menu for profile and log out -->
-            <div id="nav__profile" class="signed-in d-flex align-items-center">
-                <button class="btn d-flex" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            <div class="order-lg-last text-end">
+                <button class="btn ms-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasUser" aria-controls="offcanvasUser">
                     <p class="me-2 fw-bolder hide-mobile">Username</p>
                     <img src="https://jeffasseur-visuals.be/wp-content/uploads/2022/01/Phoenix-logo-e1647853809997.png" alt="Avatar-Ricky" class="rounded-circle me-2" style="height: 30px; width: 30px;">
                 </button>
+            </div>
 
-                <div class="offcanvas offcanvas-end" tab-index="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                    <div class="offcanvas-header">
-                        <button type="button" class="btn-close text-reset justify-content-end" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
+            <button class="navbar-toggler ms-2 order-first" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span data-bs-target="#offcanvasNavbar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                </span>
+            </button>
 
-                    <!-- code hieronder === INGELOGD -->
-                    <div class="offcanvas-body d-inline-flex flex-column logged-in">
-                        <img src="https://jeffasseur-visuals.be/wp-content/uploads/2022/01/Phoenix-logo-e1647853809997.png" alt="Avatar-Ricky" class="rounded-circle mx-auto d-block" style="height: 50px; width: 50px;">
-                        <h4 class="me-2 mb-4 fw-bolder text-center">Jef</h4>
-                        <a href="profile.php" class="text-center my-2">Profile</a>
-                        <a href="logout.php" id="btnLogout" class="btn btn-outline-secondary text-center my-2 w-50 mx-auto d-block">Log out</a>
-                    </div>
-                    <!-- code hierboven === INGELOGD -->
-
-                    <!-- code hieronder === NIET Ingelogd -->
-                    <div class="offcanvas-body d-inline-flex flex-column not-logged-in">
-                        <img src="https://jeffasseur-visuals.be/wp-content/uploads/2022/01/Phoenix-logo-e1647853809997.png" alt="Avatar-Ricky" class="rounded-circle mx-auto d-block" style="height: 50px; width: 50px;">
-                        <a href="logout.php" id="btnLogout" class="btn btn-outline-secondary text-center my-2 w-50 mx-auto d-block">Log in</a>
-                    </div>
-                    <!-- code hierboven === NIET Ingelogd -->
+            <div id="offcanvasNavbar" class="offcanvas offcanvas-start offcanvas-light" data-bs-hideresize="true" tabindex="-1" aria-labelledby="offcanvasNavbarLabel" style="visibility: hidden;" aria-hidden="true">
+                <div class="offcanvas-header">
+                    <h3 id="offcanvasLabel" class="offcanvas-title">Offcanvas Title</h3>
+                    <button class="btn-close" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">  
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="profile.php" class="nav-link">Profile</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" action="">
+                        <input class="form-control me-2" type="search" name="searchbar" id="searchbar" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-primary" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 18 18">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </button>
+                    </form>     
                 </div>
             </div>
         </div>
     </nav>
+
+    <div id="offcanvasUser" class="offcanvas offcanvas-end" tabindex="-1" aria-labelledby="offcanvasUserLabel" style="visibility: hidden;" aria-modal="true" role="dialog">
+        <div class="offcanvas-header">
+            <h3 id="offcanvasUserLabel" class="offcanvas-title">Offcanvas-title</h3>
+            <button class="btn-close" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+
+        <div class="offcanvas-body">
+            <ul class="list-group">
+                <li class="list-group-item">List group item</li>
+            </ul>
+        </div>
+    </div>
 </header>

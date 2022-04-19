72fc40beb3b5575438f7ff501b076bd1f6111f00
lienapaeps <paepsliena@gmail.com>
2022-04-19 11:57:07 +0200

    feat: get all projects with limit

--- classes/Project.php
+++ classes/Project.php
@@ -5,10 +5,19 @@ include_once(__DIR__ . "/DB.php");
 class Project
 {
     // this function gets all projects from the database
-    public static function getAll()
+    public static function getAll($start, $limit)
     {
         $conn = DB::getConnection();
-        $statement = $conn->prepare("select * from projects order by time ASC"); // oud naar niew
+        $statement = $conn->prepare("select * from projects order by time ASC limit $start, $limit"); // oud naar niew
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // this function counts the total of projects from the database
+    public static function countProjects()
+    {
+        $conn = DB::getConnection();
+        $statement = $conn->prepare("select count(id) as id from projects");
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }

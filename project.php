47f67657df8cd3c3a3f430d343c7c74ea9aaaad3
jeffasseur <r0846107@student.thomasmore.be>
2022-05-03 11:39:50 +0200

    feat: add project detail page

---
+++ project.php
@@ -0,0 +1,18 @@
+<?php 
+    include_once("bootstrap.php");
+    include_once("ProfileImgForm.inc.php");
+
+    session_start();
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vibar | Project</title>
+</head>
+<body>
+    
+</body>
+</html>

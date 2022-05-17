ac14f84260cad837310702fca893416d21d2a1ff
jeffasseur <r0846107@student.thomasmore.be>
2022-05-17 12:27:11 +0200

    fix: conflicts

--- sql/php_project.sql
+++ sql/php_project.sql
@@ -3,7 +3,6 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 17, 2022 at 10:10 AM
 -- Generation Time: May 17, 2022 at 10:23 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
@@ -201,8 +200,6 @@ DROP TABLE IF EXISTS `reported`;
 CREATE TABLE `reported` (
   `id` int(11) NOT NULL,
   `time` datetime NOT NULL,
-  `message` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL
   `message` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `content_type` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content_id` int(11) NOT NULL,
@@ -426,4 +423,4 @@ COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
